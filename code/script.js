7991aa7722136ef04885076180e3d9b7a400a210
tiawebdemo <35865533+tiaweb@users.noreply.github.com>
2020-05-17 09:15:26 +0700

    Update script.js

--- code/script.js
+++ code/script.js
@@ -1,39 +1,11 @@
-//
-// al pre tags on the page
-const pres = document.getElementsByTagName("pre");
+$(document).ready(function() {
+  textblock = $("pre code").html();
+  var lines = textblock.split("\n");
+  lines.splice(0, 1);
+  var newtext = lines.join("\n");
+  $("pre code").html(newtext);
 
-//
-// reformat html of pre tags
-if (pres !== null) {
-  for (let i = 0; i < pres.length; i++) {
-    // check if its a pre tag with a prism class
-    if (isPrismClass(pres[i])) {
-      // insert code and copy element
-      pres[i].innerHTML = `<div class="copy">copy</div><code class="${pres[i].className}">${pres[i].innerHTML}</code>`;
-    }
-  }
-}
-
-//
-// create clipboard for every copy element
-const clipboard = new Clipboard('.copy', {
-  target: trigger => {
-    return trigger.nextElementSibling;
-  } });
-
-
-//
-// do stuff when copy is clicked
-clipboard.on('success', event => {
-  event.trigger.textContent = 'copied!';
-  setTimeout(() => {
-    event.clearSelection();
-    event.trigger.textContent = 'copy';
-  }, 2000);
+  $("pre code").each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
 });
-
-//
-// helper function
-function isPrismClass(preTag) {
-  return preTag.className.substring(0, 8) === 'language';
-}
